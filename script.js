95d44ca9fc21cd9389afcb00cf41a5b878a7ac26
muhammadbadarr <102224593+muhammadbadarr@users.noreply.github.com>
2024-10-29 22:28:59 +0700

    Update script.js
    Using chatGPT's code for program's logic

--- script.js
+++ script.js
@@ -1,100 +1,103 @@
-inputsArray = []
-var isRunning = false
+let intervalCount = 1;
+let workTime = { min: 0, sec: 30 };
+let restTime = { min: 0, sec: 30 };
+let totalWithRest = 0;
+let totalWithoutRest = 0;
+let timerInterval;
 
-function initInputsData () {
-    if(inputsArray.length <= 0){
-        var txtwork_min = {
-            id:'txtwork_min', validClicks:0, nextControl:'txtwork_sec'
-        }
-        var txtwork_sec = {
-            id:'txtwork_sec', validClicks:0, nextControl:'txtwork_sec'
-        }
-        var txtrest_min = {
-            id:'txtrest_min', validClicks:0, nextControl:'txtrest_sec'
-        }
-        var txtrest_sec = {
-            id:'txtrest_min', validClicks:0, nextControl:'txtwork_min'
-        }
-
-        inputsArray[0] = txtwork_min
-        inputsArray[1] = txtwork_sec
-        inputsArray[2] = txtrest_min
-        inputsArray[3] = txtrest_sec
-    }
+function initInputsData() {
+  updateTotalTime();
 }
 
-function onSelectingTimeFocus (evt) {
-    this.select()
-
-    for(var i = 0; i < inputsArray; i++) {
-        inputsArray[i].validClicks = 0
-    }
+function onIntervalsBtnClick(value) {
+  intervalCount = Math.max(1, intervalCount + value);
+  $("#txtintervals").val(intervalCount);
+  updateTotalTime();
 }
 
-function onSelectingTime (evt) {
-    var inp = findInputObject(this.id)
-    var moded = this.value 
-    if(/^\d+$/.test(moded[moded.length-1]) == false) {
-        moded = moded.substring(0, this.value.length-1)
-        this.value = moded
-        return
-    }
-
-    if(moded.length > 2) {
-        if(moded[0] == '0') {
-            moded = moded.substring(1,3)
-            this.value = moded
-        }
-        else {
-            moded = moded.substring(0,2)
-            this.value = moded
-        }
-    }
-    else if(moded.length == 1) {
-        moded = "0" + moded
-        this.value = moded
-    }
-
-    if(evt.inputType == 'deleteContentBackward') {
-        if(inp.validClicks > 0) {
-            inp.validClicks = inp.validClicks - 1
-        }
-    }
-    else {
-        if(inp.validClicks < 2){
-            inp.validClicks++
-        }
-        if(isRunning == false){
-            setDefault()
-        }
-    }
-
-    if(inp.validClicks >= 2){
-        document.getElementById(inp.nextControl).select()
-    }
-
-    
+function onSelectingTime(input) {
+  let value = Math.max(0, parseInt(input.value) || 0);
+  input.value = value < 10 ? `0${value}` : value;
+  updateWorkRestTime();
 }
 
-function findInputObject(id) {
-    for(var i = 0;i < inputsArray.length; i++) {
-        if(inputsArray[i].id == id){
-            return inputsArray[i]
+function onSelectingTimeFocus(e) {
+  e.target.select();
+}
+
+function updateWorkRestTime() {
+  workTime.min = parseInt($("#txtwork_min").val());
+  workTime.sec = parseInt($("#txtwork_sec").val());
+  restTime.min = parseInt($("#txtrest_min").val());
+  restTime.sec = parseInt($("#txtrest_sec").val());
+  updateTotalTime();
+}
+
+function updateTotalTime() {
+  let totalWork = workTime.min * 60 + workTime.sec;
+  let totalRest = restTime.min * 60 + restTime.sec;
+
+  totalWithRest = (totalWork + totalRest) * intervalCount;
+  totalWithoutRest = totalWork * intervalCount;
+
+  $("#total_with_rest").text(formatTime(totalWithRest));
+  $("#total_without_rest").text(formatTime(totalWithoutRest));
+}
+
+function formatTime(seconds) {
+  let min = Math.floor(seconds / 60);
+  let sec = seconds % 60;
+  return `${min < 10 ? "0" + min : min} : ${sec < 10 ? "0" + sec : sec}`;
+}
+
+function startTimer() {
+  let totalWork = workTime.min * 60 + workTime.sec;
+  let totalRest = restTime.min * 60 + restTime.sec;
+  let currentInterval = 1;
+  let status = "Work";
+  let timeLeft = totalWork;
+
+  $("#status").text(`${status} Interval ${currentInterval}/${intervalCount}`);
+  updateCanvas(0);
+
+  timerInterval = setInterval(() => {
+    timeLeft--;
+
+    $("#tmr").text(formatTime(timeLeft));
+
+    if (timeLeft === 0) {
+      if (status === "Work") {
+        if (currentInterval === intervalCount) {
+          clearInterval(timerInterval);
+          alert("Workout Complete!");
+          return;
         }
+        status = "Rest";
+        timeLeft = totalRest;
+      } else {
+        status = "Work";
+        timeLeft = totalWork;
+        currentInterval++;
+      }
+      $("#status").text(`${status} Interval ${currentInterval}/${intervalCount}`);
     }
+    updateCanvas((totalWork + totalRest - timeLeft) / (totalWork + totalRest) * 100);
+  }, 1000);
 }
 
-function setDefault() {
-
+function updateCanvas(percentage) {
+  let canvas = $("#canvas")[0];
+  let ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.beginPath();
+  ctx.arc(50, 50, 45, -0.5 * Math.PI, (percentage / 100) * 2 * Math.PI - 0.5 * Math.PI);
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = "#00FF00";
+  ctx.stroke();
 }
 
-function onIntervalsBtnClick(d) {
-    var txtintervals = document.getElementById('txtintervals')
-    var curentVal = parseInt(txtintervals.value)
-    if((curentVal + d) > 0 && (curentVal + d) < 100) {
-        txtintervals.value = curentVal + d
-    }
-    if(isRunning == false) {
-        setDefault()
-    }
-}
+$("#buttonStart").on("click", function () {
+  if (timerInterval) clearInterval(timerInterval); // Stop previous timer if running
+  startTimer();
+});
